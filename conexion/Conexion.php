aaafcf2099485f80b05ed7a1a121df36af280405
SthefanyMedina <sdchuquipoma@ucvvirtual.edu.pe>
2025-10-28 09:08:06 -0500

    cambio del nombre de la bd

--- conexion/Conexion.php
+++ conexion/Conexion.php
@@ -2,7 +2,7 @@
 class Conexion {
     public static function conectar() {
         try {
-            $con = new PDO("mysql:host=localhost;port=3307;dbname=bdejemplo", "root", "");
+            $con = new PDO("mysql:host=localhost;port=3307;dbname=restauranteGrupo7", "root", "");
             $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $con;
         } catch (PDOException $e) {
