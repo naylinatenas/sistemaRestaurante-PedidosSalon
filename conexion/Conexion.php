10b0480d3e874b7149903be6cd271a4c0db00093
Naylin Acosta <atenasplasencia@gmail.com>
2025-10-29 23:07:26 -0500

    conexion edit

--- conexion/Conexion.php
+++ conexion/Conexion.php
@@ -1,13 +1,30 @@
 <?php
-class Conexion {
-    public static function conectar() {
-        try {
-            $con = new PDO("mysql:host=localhost;port=3307;dbname=restauranteGrupo7", "root", "");
-            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $con;
-        } catch (PDOException $e) {
-            die("Error de conexión: " . $e->getMessage());
+class Conexion
+{
+    private static $host = "sql213.infinityfree.com";
+    private static $user = "if0_40280307";
+    private static $pass = "3uhNCcjClfuwu";
+    private static $db   = "if0_40280307_sistemarestaurante";
+    private static $con  = null;
+
+    //    public static function conectar()
+    //    {
+    //        try {
+    //            $con = new PDO("mysql:host=localhost;port=3307;dbname=restauranteGrupo7", "root", "");
+    //            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //            return $con;
+    //        } catch (PDOException $e) {
+    //            die("Error de conexión: " . $e->getMessage());
+    //        }
+    //    }
+    public static function conectar()
+    {
+        if (self::$con == null) {
+            self::$con = new mysqli(self::$host, self::$user, self::$pass, self::$db);
+            if (self::$con->connect_error) {
+                die("❌ Error de conexión: " . self::$con->connect_error);
+            }
         }
+        return self::$con;
     }
-} 
-?>
+}
